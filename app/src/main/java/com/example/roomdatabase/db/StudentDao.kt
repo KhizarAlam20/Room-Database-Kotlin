225a503fb7e9896c62d50c83779a4c1e59fc3b32
KhizarAlam20 <khizar.alam20@gmail.com>
2024-01-20 22:44:02 +0500

    student dao update

--- app/src/main/java/com/example/roomdatabase/db/StudentDao.kt
+++ app/src/main/java/com/example/roomdatabase/db/StudentDao.kt
@@ -1,7 +1,25 @@
 package com.example.roomdatabase.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Delete
 
 @Dao
 interface StudentDao {
+
+    @Insert
+    suspend fun insertStudent(student: Student)
+
+    @Update
+    suspend fun updateStudent(student: Student)
+
+    @Delete
+    suspend fun deleteStudent(student: Student)
+
+    @Query("Select * from student_data_table")
+    fun getStudent(): LiveData<List<Student>>
+
 }
